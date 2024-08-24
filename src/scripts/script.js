e7d56e28723be4f8ca370e99d7e0905560ac4a90
Richard Poroszlay <richardporoszlay@gmail.com>
2024-08-24 12:56:34 +0200

    Create a 16x16 grid

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,10 @@
 for (let i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    div.classList.add("field");
-    document.getElementById("container").appendChild(div);
+    let row = document.createElement("div");
+    for (let i = 0; i < 16; i++) {
+        let div = document.createElement("div");
+        div.classList.add("field");
+        row.appendChild(div);
+    }
+    document.getElementById("container").appendChild(row);
 }
   
