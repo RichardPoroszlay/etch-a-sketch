9340d6daeef16e9457a08073c1be9dda83144f5a
Richard Poroszlay <richardporoszlay@gmail.com>
2024-09-17 17:49:53 +0200

    Create a grid size changer slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,20 +2,39 @@ function changeFieldColor(field) {
     field.style.backgroundColor = "black";
 }
 
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    for (let i = 0; i < 16; i++) {
-        let div = document.createElement("div");
-        div.classList.add("field");
-        row.appendChild(div);
-    }
-    document.getElementById("container").appendChild(row);
+function removeOldGrid(gridContainer) {
+    gridContainer.innerHTML = "";
 }
+
+function changeGridSize(sizeValue) {    
+    let gridContainer = document.getElementById("gridContainer");
+
+    removeOldGrid(gridContainer);
+
+    for (let i = 0; i < sizeValue; i++) {
+        let row = document.createElement("div");
+        for (let i = 0; i < sizeValue; i++) {
+            let div = document.createElement("div");
+            div.classList.add("field");
+            row.appendChild(div);
+        }
+        gridContainer.appendChild(row);
+    } 
+}
+
+const slider = document.getElementById("slider");
+let sliderOutput = document.getElementById("sliderOutput");
+sliderOutput.textContent = slider.value;
+
+slider.addEventListener("input", (event) => {
+    sliderOutput.textContent = event.target.value;
+    changeGridSize(event.target.value);
+});
 
 const fields = document.querySelectorAll(".field");
 
 fields.forEach((field) => {
-    field.addEventListener("click", () => {
+    field.addEventListener("mouseover", () => {
       changeFieldColor(field);
     });
 });
